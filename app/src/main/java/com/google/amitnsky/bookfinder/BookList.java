7e22f2b88c575bffec9015f4e301f6aed30c24db
amitnsky <amitkumary828@gmail.com>
2017-10-23 19:52:02 +0530

    Monitor network state to provide proper UI response.

--- app/src/main/java/com/google/amitnsky/bookfinder/BookList.java
+++ app/src/main/java/com/google/amitnsky/bookfinder/BookList.java
@@ -1,6 +1,9 @@
 package com.google.amitnsky.bookfinder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.LoaderManager;
@@ -9,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -28,11 +32,39 @@ public class BookList extends AppCompatActivity implements LoaderManager.LoaderC
         bundle.putString(SearchActivity.QUERY_KEY, searched);
         //set progress bar as empty view while loading data
         mLoadingStatusTextView = (TextView) findViewById(R.id.loading_status_tv);
-        mListView.setEmptyView(findViewById(R.id.progress_layout));
-        //init out loader and forceLoad
-        getSupportLoaderManager().initLoader(0, bundle, this).forceLoad();
+        if (isOnline()) {
+            mListView.setEmptyView(findViewById(R.id.progress_layout));
+            //init loader and forceLoad if device is online
+            getSupportLoaderManager().initLoader(0, bundle, this).forceLoad();
+        } else {
+            ProgressBar progressBar = (ProgressBar) findViewById(R.id.progress_bar_view);
+            progressBar.setVisibility(View.GONE);
+            mLoadingStatusTextView.setText("No Internet Connection!!");
+            mListView.setEmptyView(mLoadingStatusTextView);
+        }
     }
 
+    //helper method to check connectivity
+    private boolean isOnline() {
+        boolean isConnected;
+        ConnectivityManager connectivityManager = (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        //getActiveNetworkInfo returns null if u r not connected to any network
+        if (networkInfo == null) {
+            isConnected = false;
+        } else {
+            //isConnected should not be invoked on null object
+            //so we used if else block
+            isConnected = networkInfo.isConnected();
+        }
+        return isConnected;
+    }
+
+    /**
+     * Helper method to update UI,set up adapter.
+     *
+     * @param books arraylist
+     */
     private void updateUI(final ArrayList<Book> books) {
         mAdapter = new BookAdapter(getApplicationContext(), R.layout.list_item, books);
         mListView.setAdapter(mAdapter);
@@ -46,7 +78,7 @@ public class BookList extends AppCompatActivity implements LoaderManager.LoaderC
             }
         });
     }
-    
+
     @Override
     public Loader<ArrayList<Book>> onCreateLoader(int id, Bundle args) {
         return new BookAsyncLoader(getApplicationContext(), args.getString(SearchActivity.QUERY_KEY));
