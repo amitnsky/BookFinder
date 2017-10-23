f4fd769be6a57b7566a949c7dc3b85059d604ead
amitnsky <amitkumary828@gmail.com>
2017-10-23 19:00:01 +0530

    Refactor Code for Better UI.Show Loading while loader is loading

--- app/src/main/java/com/google/amitnsky/bookfinder/BookList.java
+++ app/src/main/java/com/google/amitnsky/bookfinder/BookList.java
@@ -9,27 +9,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
 public class BookList extends AppCompatActivity implements LoaderManager.LoaderCallbacks<ArrayList<Book>> {
-
+    ListView mListView;
+    BookAdapter mAdapter;
+    TextView mLoadingStatusTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_list);
+        mListView = (ListView) findViewById(R.id.book_list_view);
         String searched = getIntent().getCharSequenceExtra(SearchActivity.QUERY_KEY).toString();
         Bundle bundle = new Bundle(1);
         bundle.putString(SearchActivity.QUERY_KEY, searched);
+        //set progress bar as empty view while loading data
+        mLoadingStatusTextView = (TextView) findViewById(R.id.loading_status_tv);
+        mListView.setEmptyView(findViewById(R.id.progress_layout));
+        //init out loader and forceLoad
         getSupportLoaderManager().initLoader(0, bundle, this).forceLoad();
     }
 
     private void updateUI(final ArrayList<Book> books) {
-        ListView listView = (ListView) findViewById(R.id.book_list_view);
-        BookAdapter adapter = new BookAdapter(getApplicationContext(), R.layout.list_item, books);
-        listView.setAdapter(adapter);
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        mAdapter = new BookAdapter(getApplicationContext(), R.layout.list_item, books);
+        mListView.setAdapter(mAdapter);
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(books.get(position).getInfoLink()));
@@ -39,17 +46,17 @@ public class BookList extends AppCompatActivity implements LoaderManager.LoaderC
             }
         });
     }
-
+    
     @Override
     public Loader<ArrayList<Book>> onCreateLoader(int id, Bundle args) {
-
         return new BookAsyncLoader(getApplicationContext(), args.getString(SearchActivity.QUERY_KEY));
     }
 
     @Override
     public void onLoadFinished(Loader<ArrayList<Book>> loader, ArrayList<Book> data) {
-        if (data != null)
+        if (data != null) {
             updateUI(data);
+        }
     }
 
     //onLoaderreset set data field that is Books ArrayList null.
